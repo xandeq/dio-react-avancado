f34ef127ef7c8790049822a6aae723557bb30a71
xandeq <xandeq@gmail.com>
2021-03-26 08:31:15 -0300

    Aula Sobre Fragments

--- src/App.js
+++ src/App.js
@@ -1,21 +1,50 @@
-import React, { useState, useEffect } from 'react';
-import { ThemeContext, themes } from './Theme';
-import Card from './Card';
+import React, { useState, useEffect, Fragment } from "react";
+import { ThemeContext, themes } from "./Theme";
+import Card from "./Card";
+import Twitter from "./Twitter";
+
+const store = ["", "", ""];
+
+function Column() {
+  return (
+      <tr>
+        <td>tesnis</td>
+        <td>roupa</td>
+      </tr>
+  );
+}
 
 function App() {
   const [token, setToken] = useState();
 
   useEffect(() => {
     setTimeout(() => {
-      setToken('34534534534')
-    }, 4000)
+      setToken("34534534534");
+    }, 4000);
   }, [setToken]);
 
+  const renderColumns = (element, key) => {
+    <Fragment key={'column: ${key} '}>
+      <Column />
+    </Fragment>
+  };
+
   return (
-    <ThemeContext.Provider value={{...themes.primary, token }}>
-      <Card />
-    </ThemeContext.Provider>
-  )
+    // <ThemeContext.Provider value={{...themes.primary, token }}>
+    //   <Card />
+    // </ThemeContext.Provider>
+    // <>
+    //   <div>
+    //     ReactJs Avançado
+    //     <Card />
+    //   </div>
+    //   <div>
+    //     ReactJs Avançado
+    //     <Card />
+    //   </div>
+    // </>
+    <table>{store.map(renderColumns)}</table>
+  );
 }
 
 export default App;
--- src/Twitter.js
+++ src/Twitter.js
@@ -1,46 +1,47 @@
 import React, { useEffect } from "react";
 
 function Twitter() {
+  return <div></div>;
 
-  state = {
-    loading: '',
-    state: ''
-  }
+  // state = {
+  //   loading: '',
+  //   state: ''
+  // }
 
-  componentDidUpdate = (prevProps) => {
-    const { loading } = props
-    if(props.loading !== prevProps.loading){
-      console.log('componentDidUpdate loading=' + loading);
-    }
-  }
+  // componentDidUpdate = (prevProps) => {
+  //   const { loading } = props
+  //   if(props.loading !== prevProps.loading){
+  //     console.log('componentDidUpdate loading=' + loading);
+  //   }
+  // }
 
-  componentDidMount = () => {
-    const posts = props;
-    console.log("posts=" + posts);
-    console.log("props=" + props);
-  }
+  // componentDidMount = () => {
+  //   const posts = props;
+  //   console.log("posts=" + posts);
+  //   console.log("props=" + props);
+  // }
 
-  componentWillUnmount = () => {
-    console.log('Componente foi removido componentWillUnmount')
-  }
+  // componentWillUnmount = () => {
+  //   console.log('Componente foi removido componentWillUnmount')
+  // }
 
-  shouldComponentUpdate = (nextProps, nextState) => {
-    return state.tweet !== nextState.tweet;
-  }
+  // shouldComponentUpdate = (nextProps, nextState) => {
+  //   return state.tweet !== nextState.tweet;
+  // }
 
-  useEffect(() => {
-    const { posts, loading } = props
-  })
+  // useEffect(() => {
+  //   const { posts, loading } = props
+  // })
 
-  const tweet = () => {
-    setState({
-      tweet: true
-    })
-  }
+  // const tweet = () => {
+  //   setState({
+  //     tweet: true
+  //   })
+  // }
 
-  render() {
-    return <button onClick={tweet} >Click me</button>
-  }
+  
+  // return (<button onClick={tweet} >Click me</button>)
+  
 }
 
 export default Twitter;
